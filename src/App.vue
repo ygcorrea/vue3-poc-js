7a07921db1725c1ed79e09a80af75af18712c5a6
ygcorrea <g.yascorrea@gmail.com>
2022-09-30 12:07:18 -0300

    feat: :tada: Starting the project

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div>
+      <h3>Poczinha</h3>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/counter">Counter Example</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
